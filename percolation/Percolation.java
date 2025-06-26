0d21533d68c879a4d135cb042e2fd7edfe9a9ca5
Huzaifa Gohar <hafizhuzaifa444@gmail.com>
2025-06-26 11:10:56 +0500

    Add files via upload

---
+++ percolation/Percolation.java
@@ -0,0 +1,208 @@
+/* *****************************************************************************
+ *  Name:              Alan Turing
+ *  Coursera User ID:  123456
+ *  Last modified:     1/1/2019
+ **************************************************************************** */
+
+/*import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+
+public class Percolation {
+    private int N;
+    private boolean[] status;
+    private int openSites;
+    private WeightedQuickUnionUF uf;
+
+
+    public Percolation(int n) {
+        if (n <= 0) {
+            throw new IllegalArgumentException("n cannot be negative: " + n);
+        }
+        N = n;
+        uf = new WeightedQuickUnionUF(n * n + 2);
+        status = new boolean[n * n + 2];
+        status[0] = true;
+        status[n * n + 1] = true;
+
+    }
+
+    private int getIndex(int x, int y) {
+        return (x - 1) * N + y;
+
+    }
+
+    public void open(int row, int col) {
+        int n = getIndex(row, col);
+        if (n < 1 || n > N * N) {
+            throw new IllegalArgumentException("site (" + row + "," + col + ") does not exist");
+        }
+        if (!status[n]) {
+            if (!((n + 1) % N == 1)) {
+                if (status[n + 1]) {
+                    uf.union(n + 1, n);
+                }
+            }
+
+            if (!((n - 1) % N == 0)) {
+                if (status[n - 1]) {
+                    uf.union(n - 1, n);
+                }
+            }
+
+            if (n + N > N * N) {
+                uf.union((N * N) + 1, n);
+            }
+            else {
+                if (status[n + N]) {
+                    uf.union(n + N, n);
+                }
+            }
+
+            if (n - N < 0) {
+                uf.union(0, n);
+            }
+            else {
+                if (status[n - N]) {
+                    uf.union(n - N, n);
+                }
+            }
+            status[n] = true;
+            openSites++;
+        }
+
+    }
+
+    public boolean isOpen(int row, int col) {
+        int n = getIndex(row, col);
+        if (n < 1 || n > N * N) {
+            throw new IllegalArgumentException("site (" + row + "," + col + ") does not exist");
+        }
+        return status[n];
+    }
+
+    public boolean isFull(int row, int col) {
+        int n = getIndex(row, col);
+        if (n < 1 || n > N * N) {
+            throw new IllegalArgumentException("site (" + row + "," + col + ") does not exist");
+        }
+        return uf.find(n) == uf.find(0);
+    }
+
+    public int numberOfOpenSites() {
+        return openSites;
+    }
+
+    public boolean percolates() {
+        return uf.find(N * N + 1) == uf.find(0);
+    }
+}*/
+
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+
+public class Percolation {
+    private int N;
+    private boolean[] status;
+    private boolean[] statusd;
+    private int openSites;
+    private WeightedQuickUnionUF uf;
+    private WeightedQuickUnionUF ufd;
+
+
+    public Percolation(int n) {
+        if (n <= 0) {
+            throw new IllegalArgumentException("n cannot be negative: " + n);
+        }
+        N = n;
+        uf = new WeightedQuickUnionUF(n * n + 2);
+        ufd = new WeightedQuickUnionUF(n * n + 2);
+
+        status = new boolean[n * n + 2];
+        statusd = new boolean[n * n + 2];
+        status[0] = true;
+        statusd[n * n + 1] = true;
+        statusd[0] = true;
+
+    }
+
+    private int getIndex(int x, int y) {
+        return (x - 1) * N + y;
+
+    }
+
+    private void opening(int n, boolean[] status, WeightedQuickUnionUF uf, int flag) {
+
+        if (!status[n]) {
+
+            if (((n + 1) % N != 1) && status[n + 1]) {
+                uf.union(n + 1, n);
+            }
+            if (((n - 1) % N != 0) && status[n - 1]) {
+                uf.union(n - 1, n);
+            }
+            switch (flag) {
+                case 1:
+                    if (!(n + N > N * N) && status[n + N]) {
+                        uf.union(n + N, n);
+                    }
+                    if (n - N < 0) {
+                        uf.union(0, n);
+                    }
+                    else if (status[n - N]) {
+                        uf.union(n - N, n);
+                    }
+                    break;
+                case 2:
+                    if (n + N > N * N) {
+                        uf.union((N * N) + 1, n);
+                    }
+                    else if (status[n + N]) {
+                        uf.union(n + N, n);
+                    }
+                    if (n - N < 0) {
+                        uf.union(0, n);
+                    }
+                    else if (status[n - N]) {
+                        uf.union(n - N, n);
+                    }
+            }
+            status[n] = true;
+
+        }
+    }
+
+    public void open(int row, int col) {
+        int n = getIndex(row, col);
+        if (n < 1 || n > N * N) {
+            throw new IllegalArgumentException("site (" + row + "," + col + ") does not exist");
+        }
+        opening(n, status, uf, 1);
+        opening(n, statusd, ufd, 2);
+        openSites++;
+
+
+    }
+
+    public boolean isOpen(int row, int col) {
+        int n = getIndex(row, col);
+        if (n < 1 || n > N * N) {
+            throw new IllegalArgumentException("site (" + row + "," + col + ") does not exist");
+        }
+        return status[n];
+    }
+
+    public boolean isFull(int row, int col) {
+        int n = getIndex(row, col);
+        if (n < 1 || n > N * N) {
+            throw new IllegalArgumentException("site (" + row + "," + col + ") does not exist");
+        }
+        return uf.find(n) == uf.find(0);
+    }
+
+    public int numberOfOpenSites() {
+        return openSites;
+    }
+
+    public boolean percolates() {
+        return ufd.find(N * N + 1) == ufd.find(0);
+    }
+}
+
---
+++ percolation/PercolationStats.java
@@ -0,0 +1,76 @@
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
+
+public class PercolationStats {
+    private int T;
+    private double[] frOpen;
+
+
+
+
+
+
+    // perform independent trials on an n-by-n grid
+    public PercolationStats(int n, int trials){
+        T=trials;
+        frOpen=new double[T];
+        int randInt;
+        int row;
+        int col;
+
+        Percolation p;
+        for(int i=0;i<T;i++){
+
+            p=new Percolation(n);
+            while(!p.percolates()){
+                randInt = StdRandom.uniformInt(1, n * n + 1);
+                row=randInt/n+1;
+                col=randInt%n;
+                if(!p.isOpen(row,col)){
+                    p.open(row,col);
+
+
+                }
+
+
+
+            }
+            frOpen[i]=p.numberOfOpenSites()/((double)(n*n));
+        }
+
+
+    }
+
+    // sample mean of percolation threshold
+    public double mean(){
+
+        return StdStats.mean(frOpen);
+    }
+
+    // sample standard deviation of percolation threshold
+    public double stddev(){
+
+        return StdStats.stddev(frOpen);
+    }
+
+    // low endpoint of 95% confidence interval
+    public double confidenceLo(){
+        return StdStats.mean(frOpen)-(1.96*StdStats.stddev(frOpen)/(Math.sqrt(T)));
+    }
+
+    // high endpoint of 95% confidence interval
+    public double confidenceHi(){
+        return StdStats.mean(frOpen)+(1.96*StdStats.stddev(frOpen)/(Math.sqrt(T)));
+    }
+
+    // test client (see below)
+    public static void main(String[] args){
+        PercolationStats ps=new PercolationStats(Integer.parseInt(args[0]),Integer.parseInt(args[1]));
+        System.out.println("mean"+"                              "+"= "+ps.mean());
+        System.out.println("stddev"+"                            "+"= "+ps.stddev());
+        System.out.println("95% confidence interval           "+"= ["+ps.confidenceLo()+", "+ps.confidenceHi()+"]");
+
+
+    }
+
+}
